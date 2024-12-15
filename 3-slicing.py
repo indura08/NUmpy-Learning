481797a4d18d1f51d80841dc92538b6da0e6e0b2
indura08 <induraperera3@gmail.com>
2024-12-16 00:50:58 +0530

    finishing slicing part

--- 3-slicing.py
+++ 3-slicing.py
@@ -15,5 +15,28 @@ print(a[1, :])
 #get a specific column
 print(a[:, 3])  #mekn krnne 3 column ekt adlawa okkoma data tika denwa
 
+# [startIndex:endIndex:stepSize]
+print(a[0, 1:6:2])
 
+# 3d example:
+b = np.array([
+    [
+        [1,2],
+        [3,4]
+    ] ,
 
+    [
+        [5,6],
+        [7,8]
+    ]
+])
+print("printing new shaped b arrays info and other stuff")
+print(b.shape)
+print(b)
+
+#get sepcific element (work outside in)
+print(b[0,0,1])
+
+#replacings
+b[:,1,:] = [[9,9], [8,8]]
+print(b)
